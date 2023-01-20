1d708c05ba7041ea0ef323d24d0cec2dc4c272ac
WBomfim <willianbomfim@hotmail.com>
2023-01-20 12:33:36 -0300

    feat: Criado a função top_5_new - A função retorna as 5 noticias com maior número de comentários na publicação

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,6 +1,16 @@
+from tech_news.database import find_news
+
+
 # Requisito 10
 def top_5_news():
-    """Seu código deve vir aqui"""
+    news = find_news()
+    sorted_news = sorted(
+        news,
+        key=lambda
+        item: (item["comments_count"], item["title"]),
+        reverse=True
+    )
+    return [(new["title"], new["url"]) for new in sorted_news[:5]]
 
 
 # Requisito 11
