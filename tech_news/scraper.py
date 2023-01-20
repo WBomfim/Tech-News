9fbc04bef8abec9d7ad61175ce5d43bcfdeb974c
WBomfim <willianbomfim@hotmail.com>
2023-01-20 01:06:44 -0300

    feat: Criado a função get_tech_news - Responsável por salvar e retornar as ultimas notícias de acordo com o número passado como parametro.

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,6 +1,7 @@
 import requests
 from time import sleep
 from parsel import Selector
+from tech_news.database import create_news
 
 
 # Requisito 1
@@ -61,4 +62,15 @@ def scrape_news(html_content):
 
 # Requisito 5
 def get_tech_news(amount):
-    """Seu código deve vir aqui"""
+    url = "https://blog.betrybe.com"
+    news = []
+    while len(news) <= amount:
+        html_content = fetch(url)
+        links = scrape_updates(html_content)
+        for link in links:
+            news.append(scrape_news(fetch(link)))
+        url = scrape_next_page_link(html_content)
+        if not url:
+            break
+    create_news(news[:amount])
+    return news[:amount]
